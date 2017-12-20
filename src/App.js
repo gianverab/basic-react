1083ba5bc45d458fa0ead33c5e5edc1fd52c967f
Giancarlo Vera <gianverab@gmail.com>
2017-12-20 04:30:49 -0300

    update to boilerplate based on create-react-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const App = () => (
+  <Wrapper>
+    <h1>Welcome to React</h1>
+    <p>To get started, edit <code>src/App.js</code> and save to reload.</p>
+  </Wrapper>
+);
+
+export default App;
+
+const Wrapper = styled.div`
+font-family: Arial, Helvetica, sans-serif;
+text-align: center;
+`;
